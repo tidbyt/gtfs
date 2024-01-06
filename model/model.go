0e89916106e45168405f0652c8926bd5f5266f83
Mats Linander <matslina@gmail.com>
2024-01-06 13:33:38 -0500

    reorder

--- model/model.go
+++ model/model.go
@@ -5,11 +5,52 @@ import (
 	"time"
 )
 
+// Holds all external facing types and constants.
+
+type LocationType int
+
+const (
+	LocationTypeStop LocationType = iota
+	LocationTypeStation
+	LocationTypeEntranceExit
+	LocationTypeGenericNode
+	LocationTypeBoardingArea
+)
+
+type RouteType int
+
+const (
+	RouteTypeTram       RouteType = 0
+	RouteTypeSubway               = 1
+	RouteTypeRail                 = 2
+	RouteTypeBus                  = 3
+	RouteTypeFerry                = 4
+	RouteTypeCable                = 5
+	RouteTypeAerial               = 6
+	RouteTypeFunicular            = 7
+	RouteTypeTrolleybus           = 11
+	RouteTypeMonorail             = 12
+)
+
 type Agency struct {
 	ID       string
 	Name     string
 	URL      string
 	Timezone string
+}
+
+type Calendar struct {
+	ServiceID string
+	StartDate string
+	EndDate   string
+	Weekday   int8
+}
+
+type CalendarDate struct {
+	ServiceID     string
+	Date          string
+	ExceptionType int8
+	// TODO: Enum for exception types?
 }
 
 type Stop struct {
@@ -25,16 +66,6 @@ type Stop struct {
 	PlatformCode  string
 }
 
-type LocationType int
-
-const (
-	LocationTypeStop LocationType = iota
-	LocationTypeStation
-	LocationTypeEntranceExit
-	LocationTypeGenericNode
-	LocationTypeBoardingArea
-)
-
 type Trip struct {
 	ID          string
 	RouteID     string
@@ -42,6 +73,18 @@ type Trip struct {
 	Headsign    string
 	ShortName   string
 	DirectionID int8
+}
+
+type Route struct {
+	ID        string
+	AgencyID  string
+	ShortName string
+	LongName  string
+	Desc      string
+	Type      RouteType
+	URL       string
+	Color     string
+	TextColor string
 }
 
 type StopTime struct {
@@ -67,47 +110,6 @@ func (st *StopTime) DepartureTime() time.Duration {
 	return time.Duration(h)*time.Hour + time.Duration(m)*time.Minute + time.Duration(s)*time.Second
 }
 
-type Route struct {
-	ID        string
-	AgencyID  string
-	ShortName string
-	LongName  string
-	Desc      string
-	Type      RouteType
-	URL       string
-	Color     string
-	TextColor string
-}
-
-type RouteType int
-
-const (
-	RouteTypeTram       RouteType = 0
-	RouteTypeSubway               = 1
-	RouteTypeRail                 = 2
-	RouteTypeBus                  = 3
-	RouteTypeFerry                = 4
-	RouteTypeCable                = 5
-	RouteTypeAerial               = 6
-	RouteTypeFunicular            = 7
-	RouteTypeTrolleybus           = 11
-	RouteTypeMonorail             = 12
-)
-
-type Calendar struct {
-	ServiceID string
-	StartDate string
-	EndDate   string
-	Weekday   int8
-}
-
-type CalendarDate struct {
-	ServiceID     string
-	Date          string
-	ExceptionType int8
-	// TODO: Enum for exception types?
-}
-
 // Holds all Headsigns for trips passing through a stop, for a given
 // route and direction.
 type RouteDirection struct {
@@ -117,6 +119,7 @@ type RouteDirection struct {
 	Headsigns   []string
 }
 
+// A vehicle departing from a stop.
 type Departure struct {
 	StopID       string
 	RouteID      string
